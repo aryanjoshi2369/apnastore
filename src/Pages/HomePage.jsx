ecc24b2e9d608c9e83f5e1ba0ed7aad45fc44e13
Aryan <aryajnoshi033@gmail.com>
2024-11-09 17:38:38 +0530

    Aryan first commint

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const HomePage = () => {
   return (
     <>
       <h1>This is the home page by default route is /</h1>
+      <h1>system pe system</h1>
     </>
-  )
-}
+  );
+};
 
 export default HomePage;
