78425ab5bc2131f7ec30ef2194a8da5ce71197fb
Aryan <aryajnoshi033@gmail.com>
2024-11-09 18:20:32 +0530

    cleared

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 
 const HomePage = () => {
-  return (
-    <>
-      <h1>This is the home page by default route is /</h1>
-      <h1>system pe system</h1>
-      <h2>Code written by aditya</h2>
-    </>
-  );
+  return <></>;
 };
 
 export default HomePage;
