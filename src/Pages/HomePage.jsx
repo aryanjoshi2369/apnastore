2162a47a3adbe0ba3079a7b4f0005b168ae9b5c9
programmaraditya123 <officialadi4255@gmail.com>
2024-11-09 23:41:58 +0530

    9nov3

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -5,6 +5,7 @@ const HomePage = () => {
     <>
       <h1>This is the home page by default route is /</h1>
       <h1>system pe system</h1>
+      <h2>Code written by aditya</h2>
     </>
   );
 };
